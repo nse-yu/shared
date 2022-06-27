aba56889fb14a9fd2239bbcdfd605503bd633758
kakedasinoyu <nagase.programming@gmail.com>
2022-06-27 21:42:33 +0900

    create workspace

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println(19);
+        System.out.println("uf0auf00000000000000000");
     }
 }
