937d72eb8f5ec47bc3295bc4ec7e44ac3305bc6d
nse-yu <nagase.programming@gmail.com>
2022-06-27 21:38:09 +0900

    create free space to edit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println(19);
+        System.out.println("free space you edit");
     }
 }
